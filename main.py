f064829701be6e7deb2e48acefbbf9d6fa12ec41
Jules Garrett <jrgarrett3222@gmail.com>
2019-03-23 15:46:03 -0500

    reading in data

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fwd_bck import Exe
 import pprint as pp
 import pandas as pd
-import string
 import sys
 
 def main():
@@ -11,18 +10,15 @@ def main():
     obs = df.values.tolist()
     # obs = [tuple(x) for x in df.values]
     # observations = ('N','N','N','N','N','E','E','N','N','N')
+    all_words =[]
+    for i in range(10):#range(len(obs)):
+        for j in range(len(obs[i])):
+            print(obs[i][j])
+            sentence = obs[i][j].split()
+            for word in sentence:
+                all_words.append(word)
 
-    # print(df)
-    # print((tuple(obs[:][1])))
-    # pp.pprint(tuple(obs))
-    print(type(tuple(obs[0][0])))
-    obs = tuple(obs)
-    # for e in obs:
-    #     obs[e] = ''.join(obs[e])
-    # pp.pprint(obs)
-    table = str.maketrans("", "", string.punctuation)
-    print(table)
-
-    # Exe(2, obs)
+    # pp.pprint(all_words)
+    Exe(2, all_words)
 if __name__ == "__main__":
     main()
