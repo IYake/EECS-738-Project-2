e8cbaf8eb9c3659169aa5a53b63cb654421df779
Jules Garrett <jrgarrett3222@gmail.com>
2019-03-25 16:17:38 -0500

    added punc. and caps in data

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pandas as pd
 import math
 
 def main():
-    
+
     #read in the data
     col = "PlayerLine"
     df = pd.read_csv('Shakespeare_data.csv', usecols = [col])
@@ -15,13 +15,13 @@ def main():
     observations = ['N','N','N','N','N','E','E','N','N','N']
     observations = tuple(observations)
     all_words = []
-    
+
     #hyper parameters
     hidden_states = 5
     lines = 100
     for i in range(lines):#range(len(obs)):
        for j in range(len(obs[i])):
-           sentence = obs[i][j].translate(str.maketrans('', '', string.punctuation)).split()
+           sentence = obs[i][j].split()
            if sentence == []:
                continue
            if sentence[0].strip() == "SCENE":
@@ -30,7 +30,7 @@ def main():
                continue
            else:
                for word in sentence:
-                   all_words.append(word.lower())
+                   all_words.append(word)
     all_words = tuple(all_words)
     model = trainHMM(hidden_states,all_words, save_as = "model"+str(hidden_states)+str(lines))
     model = load("model"+str(hidden_states)+str(lines)+".pickle")
